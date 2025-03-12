193f42355231aef6eb4d59bd52ae0c51f5f1ec2d
Sandro Baumgartner <s.baumgartner999@gmail.com>
2025-03-12 19:42:19 +0100

    add tokio runtime

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let response = reqwest::get("https://example.com").await.unwrap();
+    let body = response.text().await.unwrap();
+    println!("Body: {}", body);
 }
